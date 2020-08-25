9b5339441105e71966a977f0ae13699246b1c48c
Ziad <ziad1991@hotmail.co.uk>
2020-08-25 14:57:52 +0100

    Implement Header component with title

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react'
 import {BrowserRouter, Route} from 'react-router-dom'
 import CoinDetailPage from './pages/CoinDetailPage'
 import CoinSummaryPage from './pages/CoinSummaryPage'
+import Header from './components/Header'
 
 const App = () => {
     return (
         <div>
             <BrowserRouter>
+            <Header />
                 <Route exact path='/' component={CoinSummaryPage}/>
             </BrowserRouter>
         </div>
