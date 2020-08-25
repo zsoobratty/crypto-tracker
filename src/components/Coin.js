4e91a353809858a869ebb5eeaf98ce86aa7a8847
Ziad <ziad1991@hotmail.co.uk>
2020-08-25 16:09:50 +0100

    Style tweak to implement delete icon

--- src/components/Coin.js
+++ src/components/Coin.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 const Coin = ({ coin }) => {
     return (
-        <Link to='/coindetail'>
+        <Link to='/coindetail' className='text-decoration-none my-1 coin'>
             <li className="coinlist-item list-group-item list-group-item-action d-flex justify-content-between align-items-center text-dark">
                 <img className='coinlist-image' src={coin.image} alt=""/>
                 <span className="text-decoration-none">{coin.current_price}</span>
