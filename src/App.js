2bdeda185a75d4587d0a84834c81267d5fc5c65b
Ziad <ziad1991@hotmail.co.uk>
2020-08-25 14:48:38 +0100

    Set up routes for components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import {BrowserRouter, Route} from 'react-router-dom'
+import CoinDetailPage from './pages/CoinDetailPage'
+import CoinSummaryPage from './pages/CoinSummaryPage'
 
 const App = () => {
     return (
         <div>
-            App
+            <BrowserRouter>
+                <Route exact path='/' component={CoinSummaryPage}/>
+            </BrowserRouter>
         </div>
     )
 }
