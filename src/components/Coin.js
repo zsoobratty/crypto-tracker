e9e3bf2d8cfbbd84f14908489382d1a787601cf7
Ziad <ziad1991@hotmail.co.uk>
2020-08-25 16:06:30 +0100

    Implement styling to coin listings

--- src/components/Coin.js
+++ src/components/Coin.js
@@ -8,8 +8,17 @@ const Coin = ({ coin }) => {
                 <img className='coinlist-image' src={coin.image} alt=""/>
                 <span className="text-decoration-none">{coin.current_price}</span>
 
-                <span className={coin.price_change_percentage_24h < 0 ? "text-danger mr-2" : "text-success mr-2"}>
+                <span className={
+                    coin.price_change_percentage_24h < 0 
+                    ? "text-danger mr-2" 
+                    : "text-success mr-2"}
+                > 
+                 {" "}
+                {coin.price_change_percentage_24h < 0 ? (
                     <i className="fas fa-sort-down align-middle mr-1"></i>
+                ) : ( 
+                    <i className="fas fa-sort-up align-middle mr-1"></i> 
+                )}
                     {coin.price_change_percentage_24h}
                 </span>
                 <i className="delete-icon far fa-times-circle text-danger"></i>
--- src/context/watchListContext.js
+++ src/context/watchListContext.js
@@ -6,7 +6,7 @@ import { useState } from "react";
 export const WatchListContext = createContext();
 
 export const WatchListContextProvider = props => {
-    const [watchList, setWatchList] = useState(['bitcoin', 'ethereum', 'ripple'])
+    const [watchList, setWatchList] = useState(['bitcoin', 'ethereum', 'ripple', 'litecoin'])
 
     return (
         <WatchListContext.Provider value={{watchList}}>
