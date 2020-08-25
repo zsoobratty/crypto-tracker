1d6438be05057b951a1edd06ce03af0183379506
Ziad <ziad1991@hotmail.co.uk>
2020-08-25 15:22:13 +0100

    API request from Coin Gecko using Axios

--- src/components/CoinList.js
+++ src/components/CoinList.js
@@ -1,14 +1,20 @@
 import React, { useEffect, useState } from 'react'
-import axios from 'axios'
+import coinGecko from '../apis/coinGecko'
 
 const CoinList = () => {
 
     const [coins, setCoins] = useState([])
 
     useEffect(() => {
-        const fetchData = () => {
-
-        }
+        const fetchData = async () => {
+            const response = await coinGecko.get('/coins/markets', {
+            params: {
+                vs_currency: 'usd',
+                ids: 'bitcoin,ethereum'
+            }
+        })
+        console.log(response.data)
+    }
 
         fetchData() 
     }, [])
